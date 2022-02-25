d4bbd0437aa941f6d16d09ed04788b6b2b06cfe0
Jorge <jorgeevangelista500@gmail.com>
2022-02-25 13:16:06 -0500

    restricted

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,22 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../secrets')
+
+
+
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if(!token){
+    next({ status:401, message: "token required" })
+  } else {
+    jwt.verify(token, JWT_SECRET, (err) => {
+      if(err){
+        next({status:401, message: "token invalid" })
+      } else {
+        next()
+      }
+    })
+  }
   /*
     IMPLEMENT
 
